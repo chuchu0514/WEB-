8cb9b64f32a0c6df1ba893dba4f09cbe3d2e369c
chuchu0514 <foxghost3@naver.com>
2026-07-14 02:30:59 +0900

    0714
    내일은 저장을 배울 거예요 오늘은 복습

--- script.js
+++ script.js
@@ -3,14 +3,40 @@ const addButton = document.getElementById("add-btn");
 const list = document.getElementById("record-list");
 
 addButton.addEventListener("click", function() {
-    const text = input.value.trim();   // 앞뒤 공백 뗀 값을 한 번만 만들어 재사용
+    const text = input.value.trim();  
 
     if (text === "") {
         return;
     }
 
     const newItem = document.createElement("li");
-    newItem.textContent = text;        // 깔끔한 값으로 저장
+    newItem.textContent = text;        
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "삭제";
+
+    deleteButton.addEventListener("click", function(){
+        newItem.remove();
+    });
+
+    newItem.appendChild(deleteButton);
     list.appendChild(newItem);
     input.value = "";
 });
+
+const feeling_input = document.getElementById("feeling-data");
+const feeling_btn = document.getElementById("feeling-btn");
+const feeling_box = document.getElementById("feeling");
+
+feeling_btn.addEventListener("click", function(){
+
+    const text = feeling_input.value.trim();
+
+    if(text === "") return;
+
+    const newItem = document.createElement("p");
+
+    newItem.textContent = text;
+    feeling_box.appendChild(newItem);
+    feeling_input.value = "";
+})
