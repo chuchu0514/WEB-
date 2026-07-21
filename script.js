4a99f665783dddc57363a5425eee6e20b3af0e5f
chuchu0514 <foxghost3@naver.com>
2026-07-21 18:30:54 +0900

    0721
    달력기능 추가

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ const list = document.getElementById("record-list");
 function saveRecords(){
     localStorage.setItem("records", JSON.stringify(records));
 }
-function renderRecord(text){
+function renderRecord(record){
     const newItem = document.createElement("li");
-    newItem.textContent = text;        
+    newItem.textContent = record.text;        
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "삭제";
     deleteButton.addEventListener("click", function(){
         newItem.remove();
 
-        const index = records.indexOf(text);
+        const index = records.indexOf(record);
         records.splice(index, 1);
         saveRecords();
     });
@@ -33,7 +33,7 @@ function renderRecord(text){
 }
 function loadRecords(){
     const saved = localStorage.getItem("records");
-    if(saved == null) return;
+    if(saved === null) return;
 
     records = JSON.parse(saved);
     records.forEach(function(record){
@@ -49,9 +49,10 @@ addButton.addEventListener("click", function() {
         return;
     }
 
-    records.push(text);
+    const record = { id: Date.now(), text: text};
+    records.push(record);
     saveRecords();
-    renderRecord(text);
+    renderRecord(record);
 
 
     input.value = "";
@@ -59,29 +60,60 @@ addButton.addEventListener("click", function() {
 
 
 // ===================================
-//  기분 기능
+//  달력 기능
 // ===================================
 
-// 요소
-const feeling_input = document.getElementById("feeling-data");
-const feeling_btn = document.getElementById("feeling-btn");
-const feeling_box = document.getElementById("feeling");
+//요소
+const calendar = document.getElementById("calendar");
+const today = new Date();
+let currentYear = today.getFullYear();
+let currentMonth = today.getMonth();
 
-// 이벤트
-feeling_btn.addEventListener("click", function(){
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
 
-    const text = feeling_input.value.trim();
+prevBtn.addEventListener("click", function(){
+    currentMonth = currentMonth - 1;
+    renderCalendar(currentYear, currentMonth);
+});
 
-    if(text === "") return;
+nextBtn.addEventListener("click", function(){
+    currentMonth = currentMonth + 1;
+    renderCalendar(currentYear, currentMonth);
+});
 
-    const newItem = document.createElement("p");
+//함수
+function renderCalendar(year, month){
+    calendar.innerHTML= "";
 
-    newItem.textContent = text;
-    feeling_box.appendChild(newItem);
-    feeling_input.value = "";
-})
+    const firstDay = new Date(year, month, 1).getDay();
+    const lastDate = new Date(year, month +1 , 0).getDate();
+    const title = document.getElementById("calendar-title");
+    title.textContent = year + "년 " + (month + 1) + "월";
+
+        //1일 앞의 빈칸 추가
+    for(let i = 0; i < firstDay; i++){
+        const emptybox = document.createElement("div");
+        emptybox.className = "day";
+        calendar.appendChild(emptybox);
+    }
+
+    //daybox 
+    for(let day = 1; day <= lastDate; day++){
+        const dayBox = document.createElement("div");
+        dayBox.className = "day";
+        dayBox.textContent = day;
+
+        dayBox.addEventListener("click", function(){
+            console.log(day + "일을 클릭했어요.");
+        });
+
+        calendar.appendChild(dayBox);
+    }
+}
 
 // ===================================
 //  실행
 // ===================================
 loadRecords();
+renderCalendar(currentYear, currentMonth);
